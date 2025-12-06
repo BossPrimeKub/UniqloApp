e2d58e247ed05f919bcdb211d03f5ab2765fa40a
bossprime <bossprime@gmail.com>
2025-12-06 16:18:48 +0700

    uniqloApp

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'model/uniqlo.dart';
+
+void main() {
+  runApp(const UniqloApp());
+}
+
+class UniqloApp extends StatelessWidget {
+  const UniqloApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+        ),
+      ),
+      home: const MyHomePage(title: 'UNIQLO SHIRT'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(widget.title)
+      ),
+      body: SafeArea(child: Container(
+        child: ListView.builder(
+          itemCount: Uniqlo.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            // itemBuilder will work as for loop to generate list item
+            // return a widget for each item list
+            return buildRecipeCard(Uniqlo.samples[index]); //call functions
+          },
+        ),
+      )
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Uniqlo uniqlo) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(uniqlo.imgUrl)),
+          Text(uniqlo.imgTitle),
+        ],
+      ),
+    );
+  }
+}
+// Hello World
+
---
+++ lib/model/uniqlo.dart
@@ -0,0 +1,70 @@
+class Uniqlo {
+  String imgUrl;
+  String imgTitle;
+
+  Uniqlo(this.imgUrl,this.imgTitle);
+
+  static List<Uniqlo> samples = [
+    Uniqlo(
+      'assets/uniqlo1.webp',
+      'Uniqlo Crew Neck Short Sleeve T-Shirt',
+    ),
+    Uniqlo(
+      'assets/uniqlo2.webp',
+      'uniqlo2',
+    ),
+    Uniqlo(
+      'assets/uniqlo3.webp',
+      'uniqlo3',
+    ),
+    Uniqlo(
+      'assets/uniqlo4.webp',
+      'uniqlo4',
+    ),
+    Uniqlo(
+      'assets/uniqlo5.webp',
+      'uniqlo5',
+    ),
+    Uniqlo(
+      'assets/uniqlo6.webp',
+      'uniqlo6',
+    ),
+    Uniqlo(
+      'assets/uniqlo7.webp',
+      'uniqlo7',
+    ),
+    Uniqlo(
+      'assets/uniqlo8.webp',
+      'uniqlo8',
+    ),
+    Uniqlo(
+      'assets/uniqlo9.webp',
+      'uniqlo9',
+    ),
+    Uniqlo(
+      'assets/uniqlo10.webp',
+      'uniqlo10',
+    ),
+    Uniqlo(
+      'assets/uniqlo11.webp',
+      'uniqlo11',
+    ),
+    Uniqlo(
+      'assets/uniqlo12.webp',
+      'uniqlo12',
+    ),
+    Uniqlo(
+      'assets/uniqlo13.webp',
+      'uniqlo13',
+    ),
+    Uniqlo(
+      'assets/uniqlo14.webp',
+      'uniqlo14',
+    ),
+    Uniqlo(
+      'assets/uniqlo15.webp',
+      'uniqlo15',
+    ),
+  ];
+}
+
